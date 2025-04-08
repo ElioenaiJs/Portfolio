96e97e9fe248009e48eac524c9a01eb8dea51af9
Elio <elio.antonio.654@gmail.com>
2025-04-08 15:31:59 -0600

    feat: Add experience component and integrate it into the layout

--- src/app/components/pages/layout/layout.component.ts
+++ src/app/components/pages/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../navbar/navbar.component";
 import { PresentacionComponent } from "../../presentacion/presentacion.component";
+import { ExperienceComponent } from "../../experience/experience.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [NavbarComponent, PresentacionComponent],
+  imports: [PresentacionComponent, ExperienceComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
