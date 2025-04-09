5d24edaadbbd809d3efd10eb82bf4d96ad28ca71
Elio <elio.antonio.654@gmail.com>
2025-04-08 21:31:26 -0600

    feat: Add AllProjects component and integrate it into the layout

--- src/app/components/pages/layout/layout.component.ts
+++ src/app/components/pages/layout/layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../navbar/navbar.component";
 import { PresentacionComponent } from "../../presentacion/presentacion.component";
 import { ExperienceComponent } from "../../experience/experience.component";
+import { AllProjectsComponent } from "../../all-projects/all-projects.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [PresentacionComponent, ExperienceComponent],
+  imports: [PresentacionComponent, ExperienceComponent, AllProjectsComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
