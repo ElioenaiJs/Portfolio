6ea3ccc0cc76072afe5a14327e248d283d9bb79a
Elio <elio.antonio.654@gmail.com>
2025-04-09 10:11:12 -0600

    feat: Add Biography component and integrate it into the layout

--- src/app/components/pages/layout/layout.component.ts
+++ src/app/components/pages/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { PresentacionComponent } from "../../presentacion/presentacion.component";
 import { ExperienceComponent } from "../../experience/experience.component";
 import { AllProjectsComponent } from "../../all-projects/all-projects.component";
+import { BiographyComponent } from "../../biography/biography.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [PresentacionComponent, ExperienceComponent, AllProjectsComponent],
+  imports: [PresentacionComponent, ExperienceComponent, AllProjectsComponent, BiographyComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
