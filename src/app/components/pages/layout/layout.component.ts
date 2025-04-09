6e10b37ac9b4ed28eac6e3abc60e903bc5adbed8
Elio <elio.antonio.654@gmail.com>
2025-04-09 12:10:17 -0600

    feat: Add Footer component and integrate it into the layout

--- src/app/components/pages/layout/layout.component.ts
+++ src/app/components/pages/layout/layout.component.ts
@@ -3,10 +3,11 @@ import { PresentacionComponent } from "../../presentacion/presentacion.component
 import { ExperienceComponent } from "../../experience/experience.component";
 import { AllProjectsComponent } from "../../all-projects/all-projects.component";
 import { BiographyComponent } from "../../biography/biography.component";
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [PresentacionComponent, ExperienceComponent, AllProjectsComponent, BiographyComponent],
+  imports: [PresentacionComponent, ExperienceComponent, AllProjectsComponent, BiographyComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
